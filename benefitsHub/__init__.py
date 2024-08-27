4e8679a640d20bfba0b3bbd00bdc1adaadf8c856
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-08-27 08:51:50 +0000

    replit commit

--- benefitsHub/__init__.py
+++ benefitsHub/__init__.py
@@ -1,5 +1,3 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
--- benefitsHub/models/base_model.py
+++ benefitsHub/models/base_model.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """Module defines a base model for users and benefits"""
-from flask import FLask
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-app = Flask(__name__)
+Base = declarative_base()
 
+engine = create_engine('mysql://user:password@host/database')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
 
 class BaseUserModel:
     """class defines base user model"""
-    id = db.Column(INTEGER)
+    id = db.Column(In)
