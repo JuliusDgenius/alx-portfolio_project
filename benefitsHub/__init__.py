bbe4f3f596f2249c3c8533a558bcd8d0b5a46f72
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-08-30 14:53:58 +0000

    login validation

--- benefitsHub/__init__.py
+++ benefitsHub/__init__.py
@@ -11,7 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///benefitsHub.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login' # The name of the route to redirect to if the user is not logged in.
+login_manager.login_message = 'info'
 
 # Import routes
 from benefitsHub import routes
--- benefitsHub/routes.py
+++ benefitsHub/routes.py
@@ -1,7 +1,7 @@
 import re
 from flask_login import login_user, current_user, logout_user, login_required
 from benefitsHub import app, db, bcrypt
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from benefitsHub.forms import RegistrationForm, LoginForm
 from benefitsHub.models.base_model import User
 
@@ -136,7 +136,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -147,6 +148,6 @@ def logout():
     return redirect(url_for('home'))
 
 @app.route('/account')
-@login_required
+@login_required # login is required to access this route.
 def account():
     return render_template('account.html', title='Account')
