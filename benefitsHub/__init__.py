495a8f60b525ea113a1f96de9174c5132227e807
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-08-30 14:57:49 +0000

    login validation

--- benefitsHub/__init__.py
+++ benefitsHub/__init__.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # The name of the route to redirect to if the user is not logged in.
-login_manager.login_message = 'info'
+login_manager.login_message_category = 'info'
 
 # Import routes
 from benefitsHub import routes
