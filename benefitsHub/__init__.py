001df19e9aa3283a8df14bf94ed768d54812115f
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-08-31 10:39:28 +0000

    update

--- benefitsHub/__init__.py
+++ benefitsHub/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_manager
 
@@ -10,6 +11,7 @@ app.config['SECRET_KEY'] = 'fdf898181ba60d610301084df980e442'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///benefitsHub.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # The name of the route to redirect to if the user is not logged in.
 login_manager.login_message_category = 'info'
