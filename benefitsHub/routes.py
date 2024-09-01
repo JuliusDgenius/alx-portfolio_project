dbe17d7e446551d391f85b419324593d2a859635
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-09-01 19:03:51 +0000

    handle benefit image

--- benefitsHub/routes.py
+++ benefitsHub/routes.py
@@ -80,6 +80,28 @@ def home():
     benefits = Benefit.query.all()
     return render_template('home.html', profile_pic=profile_pic, benefits=benefits)
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        # If the user does not select a file, the browser submits an empty file without a filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)  # Ensure the filename is safe
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect(url_for('upload_file'))
+    
+    return render_template('create_benefit.html')
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
