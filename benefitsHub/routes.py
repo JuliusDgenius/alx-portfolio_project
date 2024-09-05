6b30c49b777e018773487355e2c1fbad9a016d84
Julius Okwe <85571573+JuliusDgenius@users.noreply.github.com>
2024-09-05 07:47:11 +0000

    correct returned user object when fetching post

--- benefitsHub/routes.py
+++ benefitsHub/routes.py
@@ -222,7 +222,7 @@ def new_post():
 def user_posts(username):
     page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=user)\
+    posts = Post.query.filter_by(author=user.username)\
             .order_by(Post.date_posted.desc())\
             .paginate(page=page, per_page=10)
     return render_template('user_posts.html', posts=posts, user=user)
